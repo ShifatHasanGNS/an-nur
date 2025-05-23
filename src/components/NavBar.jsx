19f493b83c29622bc39cdd7822455f1f7339dc4d
Md. Shifat Hasan <shifathasan@MacBook-Pro-M1.local>
2025-05-23 23:00:09 +0600

    another final push

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,11 +15,11 @@ const NavBar = memo(function NavBar() {
   const { name, resultsCount } = userData;
   const { setSelectedPlan } = useContext(PlanSelectionContext);
 
-  console.log("NavBar - Session:", session);
-  console.log("NavBar - User Data:", userData);
-  console.log("NavBar - Name:", name);
-  console.log("NavBar - Results Count:", resultsCount);
-  console.log("NavBar - Error:", error);
+  // console.log("NavBar - Session:", session);
+  // console.log("NavBar - User Data:", userData);
+  // console.log("NavBar - Name:", name);
+  // console.log("NavBar - Results Count:", resultsCount);
+  // console.log("NavBar - Error:", error);
 
   return (
     <div className="navbar sticky top-0 justify-between items-center backdrop-blur-2xl shadow-[0_8px_32px_0_rgba(31,38,135,0.15)] rounded-lg p-auto mx-auto px-6 sm:px-50 bg-slate-900/20 border border-slate-800/30 hover:bg-slate-900/30 transition-all duration-300">
